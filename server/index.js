316aadba941c24893cd3ccc65a99af4cfc35548f
md-nahiduzzaman <md_nahiduzzaman@outlook.com>
2024-09-24 15:45:57 +0600

    server setup done

--- server/index.js
+++ server/index.js
@@ -1,1 +1,34 @@
-console.log("lifeline server");
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const {
+  MongoClient,
+  ServerApiVersion,
+  Timestamp,
+  ObjectId,
+} = require("mongodb");
+
+// config
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://localhost:5174"],
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+
+// test
+app.get("/", (req, res) => {
+  res.send("lifeline server is Running");
+});
+
+app.listen(port, () => {
+  console.log(`lifeline server is running on port: ${port}`);
+});
