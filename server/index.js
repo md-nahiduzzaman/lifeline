5c5cc01b15872c1822276206d0d1e978a2290f5b
md-nahiduzzaman <md_nahiduzzaman@outlook.com>
2024-09-24 15:58:12 +0600

    mongodb setup done

--- server/index.js
+++ server/index.js
@@ -28,6 +28,33 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w5tdn25.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 // test
 app.get("/", (req, res) => {
   res.send("lifeline server is Running");
