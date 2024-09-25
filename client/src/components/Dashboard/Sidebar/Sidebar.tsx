4f018998f1d6e0ea93431f6ccd0b5146c61877e5
md-nahiduzzaman <md_nahiduzzaman@outlook.com>
2024-09-25 09:11:55 +0600

    create folder structure

---
+++ client/src/components/Dashboard/Sidebar/Sidebar.tsx
@@ -0,0 +1,9 @@
+const Sidebar = () => {
+  return (
+    <div>
+      <h1>sidebar here</h1>
+    </div>
+  );
+};
+
+export default Sidebar;
---
+++ client/src/providers/AuthProvider.tsx
@@ -0,0 +1,9 @@
+const AuthProvider = () => {
+  return (
+    <div>
+      <h1>write auth AuthProvider here</h1>
+    </div>
+  );
+};
+
+export default AuthProvider;
