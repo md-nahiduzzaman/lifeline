0a52aa823b437e0e40b23cffb345ffd80f43a657
md-nahiduzzaman <md_nahiduzzaman@outlook.com>
2024-10-01 08:26:06 +0600

    ui: added dr search bar

--- client/src/pages/Dashboard/Patient/PatientAppointment.tsx
+++ client/src/pages/Dashboard/Patient/PatientAppointment.tsx
@@ -2,8 +2,75 @@ import DrCard from "../../../components/Dashboard/Card/DrCard";
 
 const PatientAppointment = () => {
   return (
-    <div>
-      <h1>this is patient appointment</h1>
+    <div className="space-y-6">
+      <div className="flex items-center p-10 bg-white border rounded-lg shadow-sm">
+        <div className="space-y-6">
+          <div className="space-y-2">
+            <h1 className="text-2xl text-[#041e49] font-semibold leading-none tracking-tight">
+              Search your doctor here
+            </h1>
+
+            <p className="text-sm text-muted-foreground">
+              You can search your doctor here
+            </p>
+          </div>
+
+          <div className="flex flex-col items-center gap-4 md:flex-row">
+            {/* Category Dropdown */}
+            <div className="relative text-right text-gray-700">
+              <select
+                name="category"
+                id="category"
+                className="inline-flex items-center gap-2 px-3 py-2 text-sm text-gray-700 transition ease-out bg-white border border-gray-200 rounded-md shadow-sm focus:outline-none hover:bg-gray-100 focus:bg-gray-100 focus:ring-1 focus:ring-gray-100 focus:ring-opacity-50"
+              >
+                <option value="">Select Category</option>
+                <option value="category1">Category 1</option>
+                <option value="category2">Category 2</option>
+              </select>
+            </div>
+
+            {/* dr search by name */}
+            <form>
+              <div className="flex items-center overflow-hidden border border-gray-300 rounded-lg  focus-within:ring focus-within:ring-opacity-40 focus-within:ring-gray-100">
+                <input
+                  className="px-6 py-2 text-sm text-gray-700 placeholder-gray-500 bg-white outline-none focus:placeholder-transparent focus:ring-0 focus:bg-gray-100"
+                  type="text"
+                  name="search"
+                  placeholder="Enter Doctor Name"
+                  aria-label="Enter Doctor Name"
+                />
+
+                <button className="mr-1 text-sm font-medium tracking-wider text-white uppercase transition-colors duration-300 transform rounded-md bg-slate-800 hover:bg-gray-300 hover:text-slate-800 focus:bg-gray-100 focus:outline-none btn btn-sm">
+                  Search
+                </button>
+              </div>
+            </form>
+
+            {/* review filter */}
+
+            <div className="relative text-right text-gray-700">
+              <select
+                name="rating"
+                id="rating"
+                className="inline-flex items-center gap-2 px-3 py-2 text-sm text-gray-700 transition ease-out bg-white border border-gray-200 rounded-md shadow-sm focus:outline-none hover:bg-gray-100 focus:bg-gray-100 focus:ring-2 focus:ring-gray-100 focus:ring-opacity-50"
+              >
+                <option value="">Filter by Rating</option>
+                <option value="1">1 Star</option>
+                <option value="2">2 Stars</option>
+                <option value="3">3 Stars</option>
+                <option value="4">4 Stars</option>
+                <option value="5">5 Stars</option>
+              </select>
+            </div>
+
+            {/* btn */}
+            <button className="px-3 py-1.5 text-white bg-slate-900 border border-gray-300 rounded-lg hover:bg-gray-200 focus:ring ">
+              Reset
+            </button>
+          </div>
+        </div>
+      </div>
+
       <div>
         <DrCard></DrCard>
       </div>
