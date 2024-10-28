5b7fc0632579ffe1a6f0faa8b587803cd3882a4e
shahadat <nayemshahadat581@gmail.com>
2024-10-28 11:20:07 +0600

    unused variable deleted

--- client/src/pages/Dashboard/Admin/PaymentDetails.tsx
+++ client/src/pages/Dashboard/Admin/PaymentDetails.tsx
@@ -1,4 +1,4 @@
-import axios from "axios"
+
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import img from '../../../assets/images/Stripe-Payment-Logo.png'
